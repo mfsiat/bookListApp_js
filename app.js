1af922320ca38756b86379554cb966e695fa72f0
MD. Nasirul Islam <nasirul.islam@northsouth.edu>
2019-03-18 22:03:54 +0600

    Created eventListener for add book

--- app.js
+++ app.js
@@ -42,6 +42,8 @@ class UI{
 
         list.appendChild(row);
     }
+
+    
 }
 // Store Class: Handles storage (Local storage stays in the browser)
 
@@ -49,5 +51,25 @@ class UI{
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 
 // Event: Add a Book
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+    // Prevent actual submit
+    e.preventDefault();
+
+    //  Get Form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    // Instantiate book 
+    const book = new Book(title, author, isbn);
+
+    // console.log(book);
+
+    // Add book to UI 
+    UI.addBookToList(book);
+
+    // Clear Fields
+    UI.clearFields();
+});
 
 // Event: Remove a Book
